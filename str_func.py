73570e11ae13fa657db6b04e50d2fca58d76d562
Aleksandr Charushin <charushinas@gmail.com>
2023-05-09 14:14:47 +0300

    changed str_func.py, added docstr def upper

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper_string(string):
+    '''Function get string and return upper string!'''
     return string.upper()
+
+
+def title_string(string):
+    '''Function get string and return title string'''
+    return string.title()
