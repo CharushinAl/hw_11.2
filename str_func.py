8f323e8131caae3cd84519a90d58798340494180
Aleksandr Charushin <charushinas@gmail.com>
2023-05-11 20:52:44 +0300

    added hello_troll and changed upper docstr

--- str_func.py
+++ str_func.py
@@ -1,8 +1,13 @@
 def upper_string(string):
-    '''Function get string and return upper string'''
+    '''Function get string and return upper string. Wtf?'''
     return string.upper()
 
 
 def title_string(string):
     '''Function get string and return title string'''
     return string.title()
+
+
+def hello_troll():
+    '''Function print hello troll'''
+    print('Hello, Troll!')
